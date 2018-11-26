d9a40f0c4e38f147ff7f3c05fc61831552b7dd82
Shreyas Thiagaraj <shreyas.thiagaraj@gmail.com>
2018-11-26 17:47:34 +0530

    Add iOS module files

---
+++ example-sdk-module/ios/RNBlockstackSdk/RNBlockstackSdk.h
@@ -0,0 +1,18 @@
+//
+//  RNBlockstackSdk.h
+//  example-sdk-module
+//
+//  Created by Shreyas Thiagaraj on 11/26/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#ifndef RNBlockstackSdk_h
+#define RNBlockstackSdk_h
+
+
+#endif /* RNBlockstackSdk_h */
+
+#import <React/RCTBridgeModule.h>
+@interface RNBlockstackSdk : NSObject <RCTBridgeModule>
+
+@end
---
+++ example-sdk-module/ios/RNBlockstackSdk/RNBlockstackSdk.m
@@ -0,0 +1,17 @@
+//
+//  RNBlockstackSdk.m
+//  example-sdk-module
+//
+//  Created by Shreyas Thiagaraj on 11/26/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RNBlockstackSdk.h"
+
+@implementation RNBlockstackSdk
+
+// To export a module named RNBlockstackSdk
+RCT_EXPORT_MODULE();
+
+@end
